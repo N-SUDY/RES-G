017bf9df078a2f9d53bd60fc55de5f39080a37ec
RadhaXRani <radha11189198141@gmail.com>
2024-09-29 08:46:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "23303247"))
+API_HASH = getenv("API_HASH", "23623f737dc15708708c65a7297e1510")
+BOT_TOKEN = getenv("BOT_TOKEN", "6398304554:AAF0shKN88TuXORWtoG7-qR_nFNXeH4lN_I")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7763435852").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://RADHAXRANI:RADHAXRANI@cluster0.ftpb4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002416068649")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002361150456"))
