90f01501d42079a0d944b0f59695b892b373b8f4
RadhaXRani <radha11189198141@gmail.com>
2024-09-29 10:53:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "23303247"))
 API_HASH = getenv("API_HASH", "23623f737dc15708708c65a7297e1510")
-BOT_TOKEN = getenv("BOT_TOKEN", "6398304554:AAF0shKN88TuXORWtoG7-qR_nFNXeH4lN_I")
+BOT_TOKEN = getenv("BOT_TOKEN", "7432362658:AAG8FE6EHXxemytm6YsAblljupkLwbif9d8")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7763435852").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://RADHAXRANI:RADHAXRANI@cluster0.ftpb4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002416068649")
